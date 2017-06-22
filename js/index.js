2dc1f398e70099d9d31ac38b54daa7099619c2e6
Michael Cullen <mwcullen@asu.edu>
2017-06-22 00:28:02 -0700

    First Commit

---
+++ js/index.js
@@ -0,0 +1,58 @@
+let shouldStop = false;
+let stopped = false;
+var downloadLink = document.getElementById('download');
+var record = document.getElementById('record');
+var stop = document.getElementById('stop');
+var audio = document.getElementById('audio');
+
+stop.disabled = true;
+
+var handleSuccess = function(stream) {
+    console.log('getUserMedia supported.');
+
+    var recordedChunks = [];
+    var mediaRecorder = new MediaRecorder(stream);
+
+    record.onclick = function() {
+        mediaRecorder.start();
+        console.log(mediaRecorder.state);
+        console.log("recorder started");
+
+        stop.disabled = false;
+        record.disabled = true;
+    }
+
+    stop.onclick = function() {
+        mediaRecorder.stop();
+        console.log(mediaRecorder.state);
+        console.log("recorder stopped");
+
+        stop.disabled = true;
+        record.disabled = false;
+    }
+
+    mediaRecorder.onstop = function(e) {
+        console.log("data available after MediaRecorder.stop() called.");
+
+        var blob = new Blob(recordedChunks, {'type':'audio/ogg; codecs=opus'})
+
+        recordedChunks = [];
+        var audioURL = window.URL.createObjectURL(blob);
+
+        audio.src = audioURL;
+
+        console.log("recorder stopped");
+
+        
+    }
+
+    mediaRecorder.ondataavailable = function(e) {
+        recordedChunks.push(e.data);
+    }
+
+};
+
+navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: false
+}).then(handleSuccess);
